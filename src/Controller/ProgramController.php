46f00c098cf4d7cf5d55abf9434df66e32766ea4
severine vanleene <severine.vanleene@gmail.com>
2023-11-29 15:50:03 +0100

    maj route program show

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,13 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/program', name: 'program_')]
 class ProgramController extends AbstractController
 {
-    #[Route('/program', name: 'program_index')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
          ]);
+    }
+
+    #[Route('/show/{id}/',methods:['GET'], requirements: ['id'=>'\d+'], name: 'show')]
+    public function show(int $id): Response
+    {
+        // Vous pouvez utiliser $id comme vous le souhaitez, par exemple, le passer à la vue.
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
+        ]);
+
     }
 }
