b4e47f11c6f119a87a27ac933ed5af671475895d
severine vanleene <severine.vanleene@gmail.com>
2023-12-04 17:11:09 +0100

    category

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -22,12 +22,28 @@ class ProgramController extends AbstractController
     }
 
     #[Route('/show/{id}/',methods:['GET'], requirements: ['id'=>'\d+'], name: 'show')]
-    public function show(int $id): Response
-    {
-        // Vous pouvez utiliser $id comme vous le souhaitez, par exemple, le passer à la vue.
-        return $this->render('program/show.html.twig', [
-            'id' => $id,
-        ]);
+    //#[Route('/show/{id<^[0-9]+$>}', name: 'show')]
 
-    }
+    public function show(int $id, ProgramRepository $programRepository):Response
+    {
+        $program = $programRepository->findOneBy(['id' => $id]);
+        // same as $program = $programRepository->find($id);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : '.$id.' found in program\'s table.'
+            );
+        }
+        return $this->render('program/show.html.twig', [
+            'program' => $program,
+        ]);
+}
+    //public function show(int $id): Response
+    //{
+        // Vous pouvez utiliser $id comme vous le souhaitez, par exemple, le passer à la vue.
+        //return $this->render('program/show.html.twig', [
+       //     'id' => $id,
+        //]);
+
+    //}
 }
