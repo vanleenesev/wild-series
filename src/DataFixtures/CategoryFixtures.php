625aa08c46433ec1b01e8f4deaa9e4fb8b2ecfe8
severine vanleene <severine.vanleene@gmail.com>
2023-12-04 11:06:30 +0100

    fixtures

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+    const CATEGORIES = [
+        'Animation',
+        'Actions',
+        'Aventures',
+        'Comedies',
+        'horeur',
+    ];
+
+    public function load(ObjectManager $manager)
+    {
+
+        foreach (self::CATEGORIES as $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+
+            $manager->persist($category);
+        }
+
+        $manager->flush();
+    }
+    
+}
